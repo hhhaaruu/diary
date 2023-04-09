3f0d2b205dc7bb01a57591a376fa9234ebfbd4e1
hhhaaruu <118385602+hhhaaruu@users.noreply.github.com>
2023-04-10 00:12:15 +0900

    削除、更新、詳細機能実施

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,7 @@
 class TweetsController < ApplicationController
+
+  before_action :set_tweet, only: [:edit, :show]
+
   def index
     @tweets = Tweet.all
   end
@@ -11,10 +14,30 @@ class TweetsController < ApplicationController
     Tweet.create(tweet_params)
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
+  def show
+  end
+
   private
 
   def tweet_params
     params.require(:tweet).permit(:name, :text, :image, :date)
   end
   
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
 end
