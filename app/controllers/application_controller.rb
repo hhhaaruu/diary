3a7917c96a32105fc318b57fe1966672afc478a9
hhhaaruu <118385602+hhhaaruu@users.noreply.github.com>
2023-04-11 02:15:57 +0900

    ユーザー機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :devise_params, if: :devise_controller?
 
 
   private
+
+  def devise_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
