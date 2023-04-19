d3ab7638b90c01ccf9ac9bc0bf7827fb72340fb9
hhhaaruu <118385602+hhhaaruu@users.noreply.github.com>
2023-04-19 20:15:53 +0900

    検索機能実施

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,4 +7,12 @@ class Tweet < ApplicationRecord
   end
   belongs_to :user
   has_many :comments
+
+  def self.search(search)
+    if search != ""
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
 end
